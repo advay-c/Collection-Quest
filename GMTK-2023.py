8525362f3e48ab03c26aa22f7aed665b01d94081
advay <134825013+advay-c@users.noreply.github.com>
2023-07-08 18:44:25 -0400

    Game over and restart button added

--- GMTK-2023.py
+++ GMTK-2023.py
@@ -13,7 +13,8 @@ BORDER = pygame.Rect(0, 0, WIDTH, HEIGHT)
 ground_image = pygame.image.load(os.path.join('gmtk-assets', 'ground_image.png'))
 PLAYER = pygame.image.load(os.path.join('gmtk-assets', 'coin.png'))
 PLAYER = pygame.transform.scale(PLAYER, (PLAYER_HEIGHT, PLAYER_WIDTH))
-game_over_text = pygame.image.load(os.path.join('imgs', 'gameover.png'))
+game_over_text = pygame.image.load(os.path.join('gmtk-assets', 'gameover.png'))
+restart = pygame.image.load(os.path.join('gmtk-assets', 'restart.png'))
 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("GMTK-2023")
@@ -37,9 +38,24 @@ class Ground(pygame.sprite.Sprite):
 
 ground = Ground(0, 0)
 
+class Button():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def is_clicked(self):
+        return pygame.mouse.get_pressed()[0] == 1 and self.rect.collidepoint(pygame.mouse.get_pos())
+
+    def draw(self, window):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+button = Button(WIDTH // 2 - 65, HEIGHT // 2 - 15, restart)
+
 player_x = 250
-player_y = 500
+player_y = 250
 gravity = 2
+game_over = False
 
 running = True
 clock = pygame.time.Clock()
@@ -52,33 +68,36 @@ while running:
 
     keys_pressed = pygame.key.get_pressed()
 
-    if keys_pressed[pygame.K_a] or keys_pressed[pygame.K_LEFT]:
-        player_x -= 5  # Move the player position to the left
+    if not game_over:
+        if keys_pressed[pygame.K_a] or keys_pressed[pygame.K_LEFT]:
+            player_x -= 5  # Move the player position to the left
 
-    if keys_pressed[pygame.K_d] or keys_pressed[pygame.K_RIGHT]:
-        player_x += 5  # Move the player position to the right
+        if keys_pressed[pygame.K_d] or keys_pressed[pygame.K_RIGHT]:
+            player_x += 5  # Move the player position to the right
 
-    if keys_pressed[pygame.K_w] or keys_pressed[pygame.K_UP]:
-        player_y -= 7
+        if keys_pressed[pygame.K_w] or keys_pressed[pygame.K_UP]:
+            player_y -= 7
 
-    player_y += gravity  # Apply constant downward displacement to the player's vertical position
+        player_y += gravity  # Apply constant downward displacement to the player's vertical position
 
-    # Check if the player has touched the ground and reset the vertical position
-    if player_y >= HEIGHT - PLAYER_HEIGHT:
-        player_y = HEIGHT - PLAYER_HEIGHT
+        # Check if the player has touched the ground and reset the vertical position
+        if player_y >= HEIGHT - PLAYER_HEIGHT:
+            game_over = True
+            player_y = HEIGHT - PLAYER_HEIGHT
 
-    # Check for collision with the border
-    if player_x < BORDER.left:
-        player_x = BORDER.left
-    elif player_x > BORDER.right - PLAYER_WIDTH:
-        player_x = BORDER.right - PLAYER_WIDTH
-    if player_y < BORDER.top:
-        player_y = BORDER.top
-    elif player_y > BORDER.bottom - PLAYER_HEIGHT:
-        player_y = BORDER.bottom - PLAYER_HEIGHT
-
-        if player_y >= BORDER:
-            WINDOW.blit(game_over_text, (200, 255))
+        # Check for collision with the border
+        if player_x < BORDER.left:
+            player_x = BORDER.left
+            game_over = True
+        elif player_x > BORDER.right - PLAYER_WIDTH:
+            player_x = BORDER.right - PLAYER_WIDTH
+            game_over = True
+        if player_y < BORDER.top:
+            player_y = BORDER.top
+            game_over = True
+        elif player_y > BORDER.bottom - PLAYER_HEIGHT:
+            player_y = BORDER.bottom - PLAYER_HEIGHT
+            game_over = True
 
     current_time = pygame.time.get_ticks()  # Get the current time in milliseconds
     WINDOW.blit(ground_image, (0, 0))
@@ -86,6 +105,15 @@ while running:
     WINDOW.blit(ground.image, ground.rect)
     pygame.draw.rect(WINDOW, (255, 255, 255), BORDER, 1)  # Draw the border
     WINDOW.blit(PLAYER, (player_x, player_y))
+
+    if game_over:
+        WINDOW.blit(game_over_text, (200, 200))
+        button.draw(WINDOW)
+
+        if button.is_clicked():
+            game_over = False
+            player_x = 250
+            player_y = 250
 
     pygame.display.update()
 
